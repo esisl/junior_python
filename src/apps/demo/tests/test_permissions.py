b5a59c999fc0866938c737eb133e9b16bacba583
esisl <esisl@yandex.ru>
2026-04-21 12:31:04 +0300

    21.04 18

--- src/apps/demo/tests/test_permissions.py
+++ src/apps/demo/tests/test_permissions.py
@@ -1,5 +1,5 @@
-# src/apps/demo/tests/test_permissions.py
 import pytest
+import json
 from django.urls import reverse
 from apps.accounts.models import User, Role, Token
 from apps.rbac.models import PermissionRule
@@ -7,7 +7,6 @@ from apps.rbac.models import PermissionRule
 @pytest.mark.django_db
 class TestArticlePermissions:
     def setup_method(self):
-        # Создаём роли и пользователей для тестов
         self.role_user = Role.objects.create(name='test_user')
         self.role_admin = Role.objects.create(name='test_admin')
         
@@ -17,30 +16,40 @@ class TestArticlePermissions:
         self.user_token = Token.create_for_user(self.user)
         self.admin_token = Token.create_for_user(self.admin)
         
-        # Базовое правило: обычный пользователь может читать статьи
         PermissionRule.objects.create(role=self.role_user, resource='article', action='read', allowed=True)
-        # Админ имеет все права через is_admin, правила не обязательны, но добавим для полноты
         PermissionRule.objects.create(role=self.role_admin, resource='article', action='create', allowed=True)
 
     def _auth_header(self, token_key):
         return {'HTTP_AUTHORIZATION': f'Token {token_key}'}
 
-    def test_user_can_read_articles(self):
-        response = self.client.get(reverse('article-list'), **self._auth_header(self.user_token.key))
+    def test_user_can_read_articles(self, client):
+        response = client.get(reverse('article-list'), **self._auth_header(self.user_token.key))
         assert response.status_code == 200
-        assert len(response.data) > 0
+        data = json.loads(response.content)
+        assert len(data) > 0
 
-    def test_user_cannot_create_articles(self):
-        response = self.client.post(reverse('article-create'), {'title': 'Hack'}, **self._auth_header(self.user_token.key))
-        assert response.status_code == 403  # Forbidden
+    def test_user_cannot_create_articles(self, client):
+        response = client.post(
+            reverse('article-create'), 
+            json.dumps({'title': 'Hack'}), 
+            content_type='application/json',
+            **self._auth_header(self.user_token.key)
+        )
+        assert response.status_code == 403
 
-    def test_admin_can_create_articles(self):
-        response = self.client.post(reverse('article-create'), {'title': 'Admin Post'}, **self._auth_header(self.admin_token.key))
+    def test_admin_can_create_articles(self, client):
+        response = client.post(
+            reverse('article-create'), 
+            json.dumps({'title': 'Admin Post'}), 
+            content_type='application/json',
+            **self._auth_header(self.admin_token.key)
+        )
         assert response.status_code == 201
-        assert response.data['title'] == 'Admin Post'
+        data = json.loads(response.content)
+        assert data['title'] == 'Admin Post'
 
-    def test_unauthenticated_user_gets_401(self):
-        response = self.client.get(reverse('article-list'))
+    def test_unauthenticated_user_gets_401(self, client):
+        response = client.get(reverse('article-list'))
         assert response.status_code == 401
 
 @pytest.mark.django_db
@@ -49,12 +58,11 @@ class TestReportPermissions:
         self.role_user = Role.objects.create(name='report_tester')
         self.user = User.objects.create_user(email='reporter@test.com', password='pass123', role=self.role_user)
         self.token = Token.create_for_user(self.user)
-        # Явно запрещаем доступ к отчётам
         PermissionRule.objects.create(role=self.role_user, resource='report', action='read', allowed=False)
 
     def _auth_header(self, token_key):
         return {'HTTP_AUTHORIZATION': f'Token {token_key}'}
 
-    def test_explicit_deny_works(self):
-        response = self.client.get(reverse('report-list'), **self._auth_header(self.token.key))
-        assert response.status_code == 403  # Запрет из PermissionRule сработал
+    def test_explicit_deny_works(self, client):
+        response = client.get(reverse('report-list'), **self._auth_header(self.token.key))
+        assert response.status_code == 403
